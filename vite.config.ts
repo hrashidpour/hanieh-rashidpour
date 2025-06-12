8f38ee4cc565ec9da5e36aec0e749dca274ad307
Hanieh Rp <hrashidpour@augusta.edu>
2025-06-11 23:12:25 -0400

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hanieh-rashidpour/',
+  base: '/hrashidpour.github.io/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
