b273c43dcc9c93e88829089a183b1af185eaaabe
Hanieh Rp <hrashidpour@augusta.edu>
2025-06-11 21:49:55 -0400

    Fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hanieh-rashidpour/',
+  build: {
+    outDir: '_site',
+    assetsDir: 'assets',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
